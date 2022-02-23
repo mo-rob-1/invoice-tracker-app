9ec5184a7193123d157f7a83163800d43d3cb572
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-02-23 18:25:17 +0000

    setup backend server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log("server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Invoice Tracker App",
+  });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Middleware
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
