ed5be25f0a1b7e610c1e707f7d839ea6da73f31f
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-09 18:31:42 +0000

    comment out required fields

--- backend/models/invoiceModel.js
+++ backend/models/invoiceModel.js
@@ -9,72 +9,72 @@ const invoiceSchema = mongoose.Schema(
     },
     userAddress: {
       type: String,
-      required: [true, "Street address is required"],
+      // required: [true, "Street address is required"],
     },
     userCity: {
       type: String,
-      required: [true, "City is required"],
+      // required: [true, "City is required"],
     },
     userPostCode: {
       type: String,
-      required: [true, "Post Code is required"],
+      // required: [true, "Post Code is required"],
     },
     userCountry: {
       type: String,
-      required: [true, "Country is required"],
+      // required: [true, "Country is required"],
     },
     clientName: {
       type: String,
-      required: [true, "Client's name is required"],
+      // required: [true, "Client's name is required"],
     },
     clientEmail: {
       type: String,
-      required: [true, "Client's email is required"],
+      // required: [true, "Client's email is required"],
     },
     clientAddress: {
       type: String,
-      required: [true, "Client's street address is required"],
+      // required: [true, "Client's street address is required"],
     },
     clientCity: {
       type: String,
-      required: [true, "Client's city is required"],
+      // required: [true, "Client's city is required"],
     },
     clientPostCode: {
       type: String,
-      required: [true, "Client's post code is required"],
+      // required: [true, "Client's post code is required"],
     },
     clientCountry: {
       type: String,
-      required: [true, "Client's country is required"],
+      // required: [true, "Client's country is required"],
     },
     invoiceDate: {
       type: Date,
-      required: [true, "Invoice date is required"],
+      // required: [true, "Invoice date is required"],
     },
     paymentTerms: {
       type: String,
-      required: [true, "Payment terms is required"],
+      // required: [true, "Payment terms is required"],
       enum: ["Net 1 Day", "Net 7 Days", "Net 14 Days", "Net 30 Days"],
     },
     description: {
       type: String,
-      required: [true, "Description is required"],
+      // required: [true, "Description is required"],
     },
     itemName: {
       type: String,
-      required: [true, "Item name is required"],
+      // required: [true, "Item name is required"],
     },
     quantity: {
       type: Number,
-      required: [true, "Quantity is required"],
+      // required: [true, "Quantity is required"],
     },
     price: {
       type: Number,
-      required: [true, "Price is required"],
+      // required: [true, "Price is required"],
     },
     status: {
       type: String,
-      required: [true, "Status is required"],
+      // required: [true, "Status is required"],
       enum: ["Pending", "Paid"],
       default: "Pending",
     },
