4047184db44b08bd07a516a0445c67d12a1d8b48
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-09 18:38:30 +0000

    Redirects user depending on their auth status

---
+++ frontend/src/hooks/useAuthStatus.js
@@ -0,0 +1,21 @@
+// Redirects user to login page if not logged in when they click on 'Create New Invoice' button / Redirects to New Invoice page if logged in.
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+    setCheckingStatus(false);
+  }, [user]);
+
+  return { loggedIn, checkingStatus };
+};
