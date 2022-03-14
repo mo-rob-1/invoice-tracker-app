9cc67b75936247498c43024d176750ee7c0f47ed
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-03-14 17:44:01 +0000

    prepare for deploy with heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -14,15 +15,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Invoice Tracker App",
-  });
-});
-
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/invoices", require("./routes/invoiceRoutes"));
+
+// Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => res.sendFile(__dirname, "../", "frontend", "build", "index.html"));
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({
+      message: "Invoice Tracker App",
+    });
+  });
+}
 
 // Middleware
 app.use(errorHandler);
